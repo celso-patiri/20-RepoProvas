73f5ddbd6bacabe9c368957454dcaa82a0ee7894
Celso <celsobenedetti2@gmail.com>
2022-07-19 08:41:18 -0300

    [NEW] schemas, header middleware, express interfaces

--- server/src/interfaces/models/index.ts
+++ server/src/interfaces/models/index.ts
@@ -1,5 +1,6 @@
+export * from "./schemas";
+
 import {
-  User,
   Term,
   Test,
   Teacher,
@@ -8,18 +9,11 @@ import {
   TeacherDiscipline,
 } from "@prisma/client";
 
-export type CreateUserDto = Omit<User, "id" | "createdAt" | "updatedAt">;
 export type CreateTermDto = Omit<Term, "id" | "createdAt" | "updatedAt">;
 export type CreateTestDto = Omit<Test, "id" | "createdAt" | "updatedAt">;
 export type CreateTeacherDto = Omit<Teacher, "id" | "createdAt" | "updatedAt">;
-export type CreateDisciplineDto = Omit<
-  Discipline,
-  "id" | "createdAt" | "updatedAt"
->;
-export type CreateCategoryDto = Omit<
-  Category,
-  "id" | "createdAt" | "updatedAt"
->;
+export type CreateDisciplineDto = Omit<Discipline, "id" | "createdAt" | "updatedAt">;
+export type CreateCategoryDto = Omit<Category, "id" | "createdAt" | "updatedAt">;
 
 export type CreateTeacherDisciplineDto = Omit<
   TeacherDiscipline,
